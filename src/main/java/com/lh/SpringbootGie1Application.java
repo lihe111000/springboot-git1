928b902fd8bdd69783d60ff4c94dfb0e249d9fad
lihe111000 <lixidong.111@qq.com>
2020-05-20 00:20:06 +0800

    初始化

---
+++ src/main/java/com/lh/SpringbootGie1Application.java
@@ -0,0 +1,13 @@
+package com.lh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGie1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGie1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lh/SpringbootGie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGie1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
